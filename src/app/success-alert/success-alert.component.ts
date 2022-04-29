05258d2e76cff63d8d03d5f608cad01784dba024
Andrew Marshall <andrewleonmarshall@gmail.com>
2022-04-28 22:40:16 -0400

    Created components for warning and success alerts

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: '<p>This was successful</p>',
+  styleUrls: ['./success-alert.component.css'],
+})
+export class SuccessAlertComponent {}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: ` <p>This is a warning</p> `,
+  styleUrls: ['./warning-alert.component.css'],
+})
+export class WarningAlertComponent {}
